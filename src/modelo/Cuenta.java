f40c03c0ca9aeb4994dde5e38f93d42744305ed6
Requisitos Marylin  Alvarado <104533185+Marylin-Alvarado@users.noreply.github.com>
2022-12-28 18:36:57 -0500

    Add files via upload

---
+++ src/modelo/Cuenta.java
@@ -0,0 +1,73 @@
+  /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author DEEPIN
+ */
+@XmlRootElement
+public class Cuenta {
+    private Integer id;
+    private String usuario;
+    private String correo;
+    private String clave;
+    private Boolean estado;
+    private Integer id_persona;
+    
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public Integer getId_persona() {
+        return id_persona;
+    }
+
+    public void setId_persona(Integer id_persona) {
+        this.id_persona = id_persona;
+    }
+    
+    
+}
---
+++ src/modelo/Matricula.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author DEEPIN
+ */
+public class Matricula extends Alumno{
+    
+    private Integer id;
+    private Date fechaEmision;
+    private Periodo periodo;
+    private Alumno alumno;
+    private List<Cursa> cursa;
+    private Integer numeroMaterias;
+    private String estado;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getFechaEmision() {
+        return fechaEmision;
+    }
+
+    public void setFechaEmision(Date fechaEmision) {
+        this.fechaEmision = fechaEmision;
+    }
+
+    public Periodo getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(Periodo periodo) {
+        this.periodo = periodo;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public List<Cursa> getCursa() {
+        return cursa;
+    }
+
+    public void setCursa(List<Cursa> cursa) {
+        this.cursa = cursa;
+    }
+
+    public Integer getNumeroMaterias() {
+        return numeroMaterias;
+    }
+
+    public void setNumeroMaterias(Integer numeroMaterias) {
+        this.numeroMaterias = numeroMaterias;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+   
+
+   
+}
